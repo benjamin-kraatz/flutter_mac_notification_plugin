607164f0d5c5940136dfbc0df5e6bed95ae2d757
Beagle Barks <benjaminkraatz@yahoo.com>
2020-02-27 17:50:33 +0100

    Initial commit

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -30,9 +30,6 @@ class _MyAppState extends State<MyApp> {
       platformVersion = 'Failed to get platform version.';
     }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted) return;
 
     setState(() {
