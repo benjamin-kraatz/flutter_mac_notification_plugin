5c4066826284ba54c3938d5b498d309e40a14053
Beagle Barks <benjaminkraatz@yahoo.com>
2020-02-27 17:57:46 +0100

    Removed platform version logic

--- lib/nocen_plugin.dart
+++ lib/nocen_plugin.dart
@@ -5,11 +5,6 @@ import 'package:flutter/services.dart';
 class NocenPlugin {
   static const MethodChannel _channel = const MethodChannel('nocen_plugin');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-
   static Future<void> get showNotification async {
     return _channel.invokeMethod('showNotification');
   }
