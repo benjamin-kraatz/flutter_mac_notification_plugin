a750b52158bed7599a00263d843fdce062383d6f
Beagle Barks <benjaminkraatz@yahoo.com>
2020-02-28 09:58:16 +0100

    Basic nocen options class to configure the notification

---
+++ lib/nocen_options.dart
@@ -0,0 +1,12 @@
+class NocenOptions {
+  final String title;
+  final String subtitle;
+  final String informative;
+  final bool hasReplyButton;
+  final String replyButtonText;
+  final bool hasActionButton;
+  final String actionButtonTitle;
+
+  NocenOptions(this.title, this.subtitle, this.informative, this.hasReplyButton,
+      this.replyButtonText, this.hasActionButton, this.actionButtonTitle);
+}
