9d17125a40890657d7a039f0a6de369ad2ec4115
Beagle Barks <benjaminkraatz@yahoo.com>
2020-02-28 10:25:25 +0100

    Some more options to customize the notification

--- lib/nocen_options.dart
+++ lib/nocen_options.dart
@@ -1,44 +1,61 @@
 import 'package:flutter/foundation.dart';
 
 class NocenOptions {
+  final String identifier;
   final String title;
   final String subtitle;
   final String informative;
   final bool hasReplyButton;
-  final String replyButtonText;
+  final String replyPlaceholder;
   final bool hasActionButton;
   final String actionButtonText;
+  final int scheduleTimeSeconds;
+  final bool schedule;
 
   NocenOptions({
+    @required this.identifier,
     @required this.title,
     @required this.subtitle,
     this.informative = '',
     this.hasReplyButton = false,
-    this.replyButtonText = '',
+    this.replyPlaceholder = '',
     this.hasActionButton = false,
     this.actionButtonText = '',
-  })  : assert(title != null, "Please give your notification a title"),
+    this.schedule = false,
+    this.scheduleTimeSeconds = 1,
+  })  : assert(
+            identifier != null, "You need an identifier for this notification"),
+        assert(title != null, "Please give your notification a title"),
         assert(subtitle != null, "Please give your notification a subtitle"),
         assert(informative != null,
             "Do not pass any null values to NocenOptions!"),
         assert(hasReplyButton != null,
             "Do not pass any null values to NocenOptions!"),
-        assert(replyButtonText != null,
+        assert(replyPlaceholder != null,
             "Do not pass any null values to NocenOptions!"),
         assert(hasActionButton != null,
             "Do not pass any null values to NocenOptions!"),
         assert(actionButtonText != null,
-            "Do not pass any null values to NocenOptions!");
+            "Do not pass any null values to NocenOptions!"),
+        assert(schedule != null,
+            "To schedule a notification, set schedule to true and scheduleTimeSeconds to a minimum of 1"),
+        assert(scheduleTimeSeconds != null,
+            "Do not pass any null values to NocenOptions!"),
+        assert(scheduleTimeSeconds >= 1,
+            "Schedule time in seconds must be greater 1 due to Apple's restrictions");
 
   Map<String, dynamic> toMap() {
     return {
+      'identifier': identifier,
       'title': title,
       'subtitle': subtitle,
       'informative': informative,
       'hasReplyButton': hasReplyButton,
-      'replyButtonText': replyButtonText,
+      'replyPlaceholder': replyPlaceholder,
       'hasActionButton': hasActionButton,
       'actionButtonText': actionButtonText,
+      'scheduleTimeSeconds': scheduleTimeSeconds,
+      'schedule': schedule,
     };
   }
 }
